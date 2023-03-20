58d0244e17aba12b31a5b80c7e4c83a94ecff72b
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-03-20 09:10:16 +0200

    edit

--- js/task-08.js
+++ js/task-08.js
@@ -2,6 +2,8 @@ const formEl = document.querySelector('.login-form');
 function onFormSubmit(event) {
   event.preventDefault();
   const { email, password } = event.currentTarget.elements;
+  // console.log('email', email);
+  // console.log('password', password);
   if (email.value === '' || password.value === '') {
     alert('Всі поля мають бути заповненні!');
     return;
@@ -10,8 +12,9 @@ function onFormSubmit(event) {
     email: email.value,
     password: password.value,
   };
-  event.currentTarget.reset();
   console.log(resultObj);
+  event.currentTarget.reset();
+
   // const resultObj = {};
   // const email = event.currentTarget.elements.email;
   // const password = event.currentTarget.elements.password;
